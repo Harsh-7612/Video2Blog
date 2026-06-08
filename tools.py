3980b982d2e369ea357d9f9bdc6cad52b2d4adf8
Harsh Kumar <hk13kumar99@gmail.com>
2026-06-08 10:32:09 +0530

    Refactor YoutubeVideoSearchTool initialization
    Updated YoutubeVideoSearchTool initialization to use a variable for the channel handle.

--- tools.py
+++ tools.py
@@ -1,4 +1,4 @@
 from crewai_tools import YoutubeVideoSearchTool
 
 # Initialize the tool with a specific yt channel handle to targeet your search
-yt_tool = YoutubeVideoSearchTool(youtube_channel_handle="@krishnaik06")
+yt_tool = YoutubeVideoSearchTool(youtube_channel_handle=youtube_channel)
